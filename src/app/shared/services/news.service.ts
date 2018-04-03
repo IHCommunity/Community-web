6f814d0bc347383c3f2652c71c746ddc7eb2bfed
Pablo Laso <plasocortabitarte@gmail.com>
2018-04-03 13:06:03 +0200

    feat: news service list get delete

--- src/app/shared/services/news.service.ts
+++ src/app/shared/services/news.service.ts
@@ -7,11 +7,27 @@ import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class NewsService extends BaseApiService {
-  private static readonly PHONES_API = `${BaseApiService.BASE_API}/news`;
+  private static readonly NEWS_API = `${BaseApiService.BASE_API}/news`;
 
   constructor(private http: Http) {
     super();
   }
 
-  
+  list(): Observable<Array<Notice>> {
+    return this.http.get(NewsService.NEWS_API, BaseApiService.defaultOptions)
+      .map((res: Response) => res.json())
+      .catch(error => this.handleError(error));
+  }
+
+  get(id: string): Observable<Notice> {
+    return this.http.get(`${NewsService.NEWS_API}/${id}`, BaseApiService.defaultOptions)
+      .map((res: Response) => res.json())
+      .catch(error => this.handleError(error));
+  }
+
+  delete(id: string): Observable<void> {
+    return this.http.delete(`${NewsService.NEWS_API}/${id}`, BaseApiService.defaultOptions)
+      .map((res: Response) => res.json())
+      .catch(error => this.handleError(error));
+  }
 }
