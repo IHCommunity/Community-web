7232d4a34aff6fb0cad0f8fc49fd40ffa90fb295
Pablo Laso <plasocortabitarte@gmail.com>
2018-04-11 11:16:54 +0200

    feat: logout

--- src/app/components/my-home/my-home.component.ts
+++ src/app/components/my-home/my-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionService } from '../../shared/services/session.service';
 
 @Component({
   selector: 'app-my-home',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private sessionService: SessionService) { }
 
   ngOnInit() {
+  }
+
+  onClickLogout() {
+    this.sessionService.logout()
+      .subscribe( () => {
+        this.router.navigate(['/login']);
+      });
   }
 
 }
