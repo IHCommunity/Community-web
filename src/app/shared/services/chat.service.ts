559bf5cd27319f66a9b61f9cd1a7efeaf4aa865b
Marco <marco238cuba@gmail.com>
2018-04-13 13:53:28 +0200

    feat: send & receive messages

--- src/app/shared/services/chat.service.ts
+++ src/app/shared/services/chat.service.ts
@@ -7,7 +7,33 @@ import { Message } from '../model/message.model';
 @Injectable()
 export class ChatService {
   private itemsCollection: AngularFirestoreCollection<Message>;
+  chats: Message[] = [];
 
   constructor(private afs: AngularFirestore, private sessionService: SessionService) {}
 
+  loadMessajes() {
+      this.itemsCollection = this.afs.collection<Message>('chats', ref => ref.orderBy('date', 'desc').limit(20));
+
+      return this.itemsCollection.valueChanges()
+                                 .map(messages => {
+                                     this.chats = [];
+
+                                     for(let msg of messages) {
+                                         this.chats.unshift(msg);
+                                     }
+                                     return this.chats;
+                                 })
+  }
+
+  addMessage(text: string, id: string) {
+      const message = {
+          senderId: this.sessionService.user.id,
+          receiverId: id,
+          userName: this.sessionService.user.name,
+          body: text,
+          date: new Date().getTime()
+      }
+
+      return this.itemsCollection.add(message);
+  }
 }
