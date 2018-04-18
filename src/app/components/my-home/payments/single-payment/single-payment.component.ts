3bc4ee375eee04cfcf5eea331538b98e572a88d1
Marco <marco238cuba@gmail.com>
2018-04-18 03:25:46 +0200

    feat: paypal payments working ok

--- src/app/components/my-home/payments/single-payment/single-payment.component.ts
+++ src/app/components/my-home/payments/single-payment/single-payment.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Payment } from '../../../../shared/model/payment.model';
 import { PaymentsService } from '../../../../shared/services/payments.service';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-single-payment',
@@ -13,6 +14,7 @@ export class SinglePaymentComponent implements OnInit {
 
   constructor(
       private routes: ActivatedRoute,
+      private router: Router,
       private paymentsService: PaymentsService) { }
 
   ngOnInit() {
@@ -22,4 +24,11 @@ export class SinglePaymentComponent implements OnInit {
           this.paymentsService.get(params['id']).subscribe( payment => this.payment = payment );
         });
     }
+
+  pay(id: string) {
+      this.paymentsService.pay(id)
+        .subscribe( login_url => {
+          window.location.href=login_url;
+        });
+  }
 }
