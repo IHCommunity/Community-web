c1146999c295c2b0fd05188be740e3e0c537b8a7
Pablo Laso <plasocortabitarte@gmail.com>
2018-04-19 21:46:59 +0200

    fix: prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  baseApi: 'https://community-ih.herokuapp.com/'
+  baseApi: 'https://community-ih.herokuapp.com/',
+  firebase: {
+    apiKey: "AIzaSyBRR_Y8h3uw5gI4X0L-PvQJcA0Ui4YXTS8",
+    authDomain: "community-200317.firebaseapp.com",
+    // databaseURL: "https://community-200317.firebaseio.com",
+    databaseURL: "https://localhost:3000",
+    projectId: "community-200317",
+    storageBucket: "community-200317.appspot.com",
+    messagingSenderId: "154953353338"
+}
 };
